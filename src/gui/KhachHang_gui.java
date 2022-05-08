004fc0c91e986b25cd142a778e3cee968f7fae4a
hvcong <hvcong101201@gmail.com>
2022-05-08 19:29:39 +0700

    done customer

--- src/gui/KhachHang_gui.java
+++ src/gui/KhachHang_gui.java
@@ -12,6 +12,8 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
+import connectdb.ConnectDB;
+import dao.KhachHang_dao;
 //import org.jdesktop.swingx.prompt.PromptSupport;
 //
 //import dao.KhachHangDAO;
@@ -33,10 +35,15 @@ import javax.swing.JOptionPane;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.awt.FlowLayout;
 import java.awt.Dimension;
 import java.awt.Color;
@@ -50,7 +57,7 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 
-public class KhachHang_gui extends JFrame {
+public class KhachHang_gui extends JFrame implements ActionListener, MouseListener, KeyListener {
 
 	private JPanel contentPane;
 	private JPanel out;
@@ -58,10 +65,12 @@ public class KhachHang_gui extends JFrame {
 	private JTable tblKhachHang;
 	private JTextField txtMaKh,txtTenKh,txtSdt;
 	private DefaultTableModel model;
-	private List<NhanVien> dskh;  
-	private JButton btnSuaKh,btnLamMoi,btnTimKiem,btnLayToanBoDuLieu;
-	private DefaultComboBoxModel cboLoaiTimKiem;
-	private JComboBox cmbLoaiTimKiem;
+	private ArrayList<KhachHang> dskh;  
+	private JButton btnSuaKh,btnLayToanBoDuLieu,btnLuu;
+	private DefaultComboBoxModel<String> cboLoaiTimKiem;
+	private JComboBox<String> cmbLoaiTimKiem;
+	
+	private KhachHang_dao kh_dao;
 	
 
 	/**
@@ -85,6 +94,15 @@ public class KhachHang_gui extends JFrame {
 	 * @throws SQLException 
 	 */
 	public KhachHang_gui() throws SQLException {
+		// khởi tạo kết nối đến CSDL
+				try {
+					new connectdb.ConnectDB().connect();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		kh_dao = new KhachHang_dao();	
+		
 		setTitle("Khách hàng");
 		setResizable(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -121,20 +139,20 @@ public class KhachHang_gui extends JFrame {
 		JPanel pnThongTinKh = new JPanel();
 		pnLeft.add(pnThongTinKh);
 		pnThongTinKh.setLayout(new BoxLayout(pnThongTinKh, BoxLayout.Y_AXIS));
-		
-		Component verticalStrut_2 = Box.createVerticalStrut(35);
-		pnThongTinKh.add(verticalStrut_2);
+//		
+//		Component verticalStrut_2 = Box.createVerticalStrut(35);
+//		pnThongTinKh.add(verticalStrut_2);
 		
 		JPanel pnTieuDe = new JPanel();
 		pnThongTinKh.add(pnTieuDe);
 		
-		JLabel lblTieuDe = new JLabel("Thông tin Khách hàng");
-		lblTieuDe.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		JLabel lblTieuDe = new JLabel("Chỉnh sửa thông tin");
+		lblTieuDe.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		pnTieuDe.add(lblTieuDe);
 		
 		Component verticalStrut_1 = Box.createVerticalStrut(20);
 		pnThongTinKh.add(verticalStrut_1);
-		
+	
 		JPanel pnMaKh = new JPanel();
 		FlowLayout fl_pnMaKh = (FlowLayout) pnMaKh.getLayout();
 		fl_pnMaKh.setAlignment(FlowLayout.LEFT);
@@ -192,15 +210,17 @@ public class KhachHang_gui extends JFrame {
 		btnSuaKh = new JButton("Sửa");
 		btnSuaKh.setBackground(Color.WHITE);
 		btnSuaKh.setIcon(new ImageIcon("data\\images\\repairing-service.png"));
+		btnSuaKh.setEnabled(false);
 //		btnSuaKh.setIconTextGap(30);
 		pnChucNang.add(btnSuaKh);
-
 		
-		btnLamMoi = new JButton("Làm mới");
-		btnLamMoi.setBackground(Color.WHITE);
-		btnLamMoi.setIcon(new ImageIcon("data\\images\\refresh.png"));
-//		btnLamMoi.setIconTextGap(10);
-		pnChucNang.add(btnLamMoi);
+		btnLuu = new JButton("Lưu");
+		btnLuu.setBackground(Color.WHITE);
+		btnLuu.setIcon(new ImageIcon("data\\images\\repairing-service.png"));
+		btnLuu.setEnabled(false);
+//		btnLuu.setIconTextGap(30);
+		pnChucNang.add(btnLuu);
+		
 		
 		JPanel pnRight = new JPanel();
 		contentPane.add(pnRight);
@@ -212,30 +232,34 @@ public class KhachHang_gui extends JFrame {
 				new BevelBorder(BevelBorder.LOWERED, null, null, null, null)));
 		pnRight.add(pnTimKiem, BorderLayout.NORTH);
 
+		JLabel lblKieuTimKiem = new JLabel("Tìm kiếm theo:");
+		pnTimKiem.add(lblKieuTimKiem);
+		
 		cboLoaiTimKiem = new DefaultComboBoxModel<String>();
-		cmbLoaiTimKiem = new JComboBox(cboLoaiTimKiem);
+		cmbLoaiTimKiem = new JComboBox<String>(cboLoaiTimKiem);
 		cmbLoaiTimKiem.setToolTipText("tìm kiếm theo");
 		cmbLoaiTimKiem.setBackground(Color.WHITE);
 		cmbLoaiTimKiem.setPreferredSize(new Dimension(130, 22));
 		pnTimKiem.add(cmbLoaiTimKiem);
 		cboLoaiTimKiem.addElement((String) "Tên KH");
 		cboLoaiTimKiem.addElement((String) "Số điện thoại");
+		
+		
+		JLabel lblTimKiem = new JLabel("Nhập giá trị tìm kiếm:");
+		lblTimKiem.setPreferredSize(new Dimension(130, 25));
+		pnTimKiem.add(lblTimKiem);
 				
 		txtNhapLieu = new JTextField();
 		txtNhapLieu.setPreferredSize(new Dimension(7, 25));
 		pnTimKiem.add(txtNhapLieu);
-		txtNhapLieu.setColumns(30);
+		txtNhapLieu.setColumns(20);
 		
-		btnTimKiem = new JButton("Tìm kiếm");
-		btnTimKiem.setPreferredSize(new Dimension(130, 25));
-		btnTimKiem.setBackground(Color.WHITE);
-		btnTimKiem.setIcon(new ImageIcon("data\\images\\search_16.png"));
-		pnTimKiem.add(btnTimKiem);
 		
-		btnLayToanBoDuLieu = new JButton("Lấy tất cả");
+		
+		btnLayToanBoDuLieu = new JButton("Xem tất cả");
 		btnLayToanBoDuLieu.setPreferredSize(new Dimension(130, 25));
 		btnLayToanBoDuLieu.setBackground(Color.WHITE);
-		btnLayToanBoDuLieu.setIcon(new ImageIcon("data\\images\\search_16.png"));
+		btnLayToanBoDuLieu.setIcon(new ImageIcon("data\\images\\refresh.png"));
 		pnTimKiem.add(btnLayToanBoDuLieu);
 		
 		
@@ -250,69 +274,33 @@ public class KhachHang_gui extends JFrame {
 		scrTableNhanVien.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrTableNhanVien.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		pnTableKh.add(scrTableNhanVien);
-		
 
-		
+		// add event
+		btnLayToanBoDuLieu.addActionListener(this);
+		btnSuaKh.addActionListener(this);
+		tblKhachHang.addMouseListener(this);
+		btnLuu.addActionListener(this);
+		txtNhapLieu.addKeyListener(this);
+		// load data from database
+		loadAllData();
 		renderData();
-		setDisable();
+		setEnabledEditForm(false);
 		
-		addEvents();
+	}
+	
+	private void loadAllData() {
+		dskh = kh_dao.getAll();
 	}
 
 	private void renderData() {
 		// TODO Auto-generated method stub
-		
-	}
-
-	private void addEvents() {
-		// TODO Auto-generated method stub
-		tblKhachHang.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-			
-			@Override
-			public void valueChanged(ListSelectionEvent e) {
-				int idx = tblKhachHang.getSelectedRow();
-				if(idx == -1 && txtTenKh.getText().equals("")) {
-					setDisable();
-				}
-				if(idx != -1) {
-					setEnable();
-					
-					
-				}
+		if(!Objects.isNull(dskh)) {
+			model.setRowCount(0);
+			for(KhachHang kh: dskh) {
+				model.addRow(new Object [] {kh.getMaKhachHang(), kh.getTenKhachHang(), kh.getSoDienThoai()});
 			}
-		});
+		}
 		
-		btnSuaKh.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				tblKhachHang.clearSelection();
-				
-				int maNV = Integer.parseInt(txtMaKh.getText());
-				String tenNV = txtTenKh.getText();
-				String sdt = txtSdt.getText();
-				
-				if(tenNV.equals("")|| kiemTraSo(tenNV)) {
-					JOptionPane.showMessageDialog(contentPane, "Tên không hợp lệ");
-					return;
-				}
-				
-				boolean ktSdt  = sdt.matches("^(0?)(3[2-9]|5[6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])[0-9]{7}$");	
-				if(ktSdt == false) {
-					JOptionPane.showMessageDialog(contentPane, "Số điện thoại không hợp lệ");
-					return;
-				}
-
-				setDisable();
-			}
-		});
-	}
-	
-	public void setDisable() {
-		btnSuaKh.setEnabled(false);
-	}
-	public void setEnable() {
-		btnSuaKh.setEnabled(true);
 	}
 
 	
@@ -329,4 +317,130 @@ public class KhachHang_gui extends JFrame {
 	public JPanel getContentPane() {
 		 return this.out;
 	 }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object o = e.getSource();
+		if(o.equals(btnSuaKh)) {
+			if(btnSuaKh.getText().equals("Hủy")) {
+				setEnabledEditForm(false);
+				int r = tblKhachHang.getSelectedRow();
+				if(r == -1) return;
+				txtMaKh.setText(tblKhachHang.getValueAt(r, 0).toString());
+				txtTenKh.setText(tblKhachHang.getValueAt(r, 1).toString());
+				txtSdt.setText(tblKhachHang.getValueAt(r, 2).toString());
+				
+			}else {
+				setEnabledEditForm(true);
+				txtTenKh.requestFocus();
+				btnSuaKh.setText("Hủy");				
+			}
+			
+		}else if(o.equals(btnLuu)) {
+			KhachHang kh = new KhachHang(Integer.parseInt(txtMaKh.getText()), txtTenKh.getText(), txtSdt.getText());
+			boolean isSuccess = kh_dao.update(kh);
+			if(isSuccess) {
+				JOptionPane.showMessageDialog(null, "Cập nhật thành công");
+				loadAllData();
+				renderData();
+				return;
+			}else {
+				JOptionPane.showMessageDialog(null, "Cập nhật không thành công, vui lòng thử lại");
+			}
+		}else if(o.equals(btnLayToanBoDuLieu)) {
+			txtNhapLieu.setText("");
+			loadAllData();
+			renderData();
+			
+		}
+	}
+	
+	private void setEnabledEditForm(boolean is) {
+		txtTenKh.setEnabled(is);
+		txtSdt.setEnabled(is);
+		btnLuu.setEnabled(is);
+		
+		if(!is) {
+			btnSuaKh.setText("Sửa");
+		}
+	
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		int r = tblKhachHang.getSelectedRow();
+		if(r == -1) return;
+		
+		setEnabledEditForm(false);
+		btnSuaKh.setEnabled(true);
+		
+		
+		
+		String maString = tblKhachHang.getValueAt(r, 0).toString();
+		String tenString = tblKhachHang.getValueAt(r, 1).toString();
+		String sdt = tblKhachHang.getValueAt(r, 2).toString();
+		
+		txtMaKh.setText(maString);
+		txtTenKh.setText(tenString);
+		txtSdt.setText(sdt);
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		Object o = e.getSource();
+		
+		String type = (String) cboLoaiTimKiem.getSelectedItem();
+		String value = txtNhapLieu.getText();
+		
+		if(type.equals("Tên KH")) {
+			dskh = kh_dao.findListKH(value, "tenKhachHang");
+			renderData();
+		}else if(type.equals("Số điện thoại")) {
+			dskh = kh_dao.findListKH(value,"soDienThoai");
+			renderData();
+		}
+		
+		
+	}
 }
